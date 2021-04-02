cab188227ff6f021b5f648b62de952d5105723ba
Timothy Quach <timq720@gmail.com>
2021-04-01 17:20:02 -0700

    Adjusted Sprite Sizes and Added Dongle Level

--- collegeGameJam2021/Assets/Scripts/Entities/Movement.cs
+++ collegeGameJam2021/Assets/Scripts/Entities/Movement.cs
@@ -1,4 +1,4 @@
-// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Movement.inputactions'
+// GENERATED AUTOMATICALLY FROM 'Assets/Scripts/Entities/Movement.inputactions'
 
 using System;
 using System.Collections;
