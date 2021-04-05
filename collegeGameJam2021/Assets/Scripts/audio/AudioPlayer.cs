76f06ba9e4f6bece8f15f8dbb3320ef1af7988aa
Michelle Wang <zhiyimichelle@gmail.com>
2021-04-04 17:55:07 -0700

    added basics of audio script

---
+++ collegeGameJam2021/Assets/Scripts/audio/AudioPlayer.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(AudioSource))]
+public class AudioPlayer : MonoBehaviour
+{
+    [SerializeField] AudioClip onCollisionClip;
+    [SerializeField] AudioClip onSpriteSwitchClip;
+    private AudioSource audioSource;
+    [SerializeField] ResultType result; 
+
+
+    private void Awake() {
+        audioSource = GetComponent<AudioSource>();
+    }
+
+    public void playCollisionAudio()
+    {
+        audioSource.PlayOneShot(onCollisionClip);
+    }
+     public void playSpriteChangeAudio()
+    {
+        audioSource.PlayOneShot(onSpriteSwitchClip);
+    }
+}
